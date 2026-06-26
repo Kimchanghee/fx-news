d1324360d5b5cec373909a6f4f8eb704df252d3c
Kimchanghee <k931103@gmail.com>
2026-06-26 12:22:22 +0900

    seo: move locale html attrs into localized root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html>
-      <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BPXLVL39BE" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-BPXLVL39BE',{page_path:window.location.pathname});",
-          }}
-        />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+  return children;
 }
